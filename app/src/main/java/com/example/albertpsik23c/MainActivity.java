7635def3857af64e6e751268d39e8085424cec65
ASUS <albertramadan4@gmail.com>
2024-08-26 09:51:58 +0700

    apk

--- app/src/main/java/com/example/albertpsik23c/MainActivity.java
+++ app/src/main/java/com/example/albertpsik23c/MainActivity.java
@@ -1,24 +1,66 @@
 package com.example.albertpsik23c;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    Button bprofil, bakad, bhobi,bcontact;
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+
+        bprofil = findViewById(R.id.btnprofil);
+        bakad= findViewById(R.id.btnakad);
+        bhobi = findViewById(R.id.btnhobi);
+        bcontact= findViewById(R.id.btncontact);
+
+
+
+        bprofil.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, Profil.class);
+                startActivity(intent);
+            }
         });
+
+        bakad.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, Akademik.class);
+                startActivity(intent);
+            }
+        });
+
+        bhobi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, Hobi.class);
+                startActivity(intent);
+            }
+        });
+        bcontact.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, Contact.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+
+
+
     }
 }
